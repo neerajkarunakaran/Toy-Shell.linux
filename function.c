5b43dabca9c8c8e74db89b38fd1363a3134f7d0f
neerajkarunakaran <neerajkarunakaran.c@gmail.com>
2017-05-16 14:13:03 +0530

    function.c

--- function.c
+++ function.c
@@ -3,17 +3,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "myshell.h"
- char *read_inputline(void)
+#include <sys/types.h>
+#include <wait.h>
+ char *read_inputline(void)  //read input lines
 {
     char *line = NULL;
     ssize_t bufsize = 0;
-    getline(&line, &bufsize, stdin);
+    getline(&line, &bufsize, stdin);  //get null terminated string
     return line;
 }
 
 char **convert_cmd(char *line)
 {
-    int bufsize =  TOK_BUF_SIZE;
+    int bufsize =  TOK_BUF_SIZE;  // commands buffer
     int position = 0;
 
     char **tokens = malloc(bufsize * sizeof(char*));
@@ -24,35 +26,38 @@ char **convert_cmd(char *line)
         exit(EXIT_FAILURE);
     }
 
-    token = strtok(line, "\t\r\n\a");
+    token = strtok(line, "\t\a\r' '\n");  //dividing the input string to commands
     while(token != NULL) {
         tokens[position] = token;
         position++;
 
-        if(position >= bufsize){
+        if(position >= bufsize){  //checking the buf size limit 
+
             bufsize += TOK_BUF_SIZE;
-            tokens = realloc(tokens, bufsize * sizeof(char*));
+            tokens = realloc(tokens, bufsize * sizeof(char*));  //allocating additional space
             if(!tokens) {
                 fprintf(stderr, "myshell: allocation error \n");
                 exit(EXIT_FAILURE);
             }
         }
     
-        token = strtok(NULL, "\t\r\n\a");
+        token = strtok(NULL, "\t\r' '\n\a");
     }
     tokens[position] = '\0';
+    argc = position;  //arguments count
+
     return tokens;
 }
 
 
-int execute_cmd(char **args)
+int execute_cmd(char **args) // execute the commands
 {
     pid_t pid, wpid;
     int status;
 
-    pid = fork();
+    pid = fork();  //new process
     if(pid == 0) {
-        if(execvp(args[0], args) == -1) {
+        if(execvp(args[0], args) == -1) { //exec command
             perror("myshell");
         }
         exit(EXIT_FAILURE);
@@ -61,10 +66,11 @@ int execute_cmd(char **args)
         perror("myshell");
     }
     else {
-        wait();
+        wait(&status);  //suspend parent process
+
     }
 
-    return 1;
+
 }
 
 
