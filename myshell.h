ad0b7411f61d749e864a6fd361a7fed2e566b9ef
neerajkarunakaran <neerajkarunakaran.c@gmail.com>
2017-05-16 18:31:29 +0530

    myshell.h

--- myshell.h
+++ myshell.h
@@ -1,13 +1,14 @@
 char *get_work_directory();
 void init_sh(void);
-#define BUFSIZE 64
-#define TOK_BUF_SIZE  64
+#define BUFSIZE  100
+#define TOK_BUF_SIZE  100
 char *read_inputline(void);
 char **convert_cmd(char *line);
-
+#include <unistd.h>
 int execute_cmd(char **args);
-
-
-
-
-
+char *get_work_directory(void);
+void select_cmd(char **args);
+int io_redirection(char **args);
+int fd;
+int argc;
+pid_t pid;
