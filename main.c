2aa8d1f4caa49703810ff4aa54cba885ad3305e9
neerajkarunakaran <neerajkarunakaran.c@gmail.com>
2017-05-16 14:13:35 +0530

    main.c

--- main.c
+++ main.c
@@ -3,24 +3,31 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#include "myshell.h"
+#include "myshell.h"  // myshell header
 int main()
 {
 
-    char *line, **args;
-    int status;
-    init_sh();
+
+    int fd;  // file descriptor 
+    char *line, **args;   //
+
+    init_sh();   //initialise  myshell
     do {
 
 
-       printf("Myshell:-%s:>>", get_work_directory());
-       line = read_inputline();
-       args = convert_cmd(line);
-       execute_cmd(args);
+       printf("Myshell:-%s:>>", get_work_directory());  //myshell prompt 
+       line = read_inputline();    //commands read from user
 
-       free(line);
+       args = convert_cmd(line);   // convert user commands into arguments vector
+
+
+        io_redirection(args);  // check the presents of io redirection symbol in args and redirect
+
+       select_cmd(args);  //check for inbuilt command and execute    
+       close(fd);  //closing file
+       free(line);  //freeing line and args
        free(args);
-    } while (1);
+    } while (1);  //loop to another input
 }
 
 
