f22d7bf28326d643620b9da96c8e9c2fc9b20f08
neerajkarunakaran <neerajkarunakaran.c@gmail.com>
2017-06-28 02:15:48 +0530

    final comitt

--- mysh/process_creator.c
+++ mysh/process_creator.c
@@ -3,115 +3,39 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include <string.h> 
-#include "myshell.h" 
-
-
-
+#include <string.h>
+#include "myshell.h"
 /* convert line input into processes and its commands */
- int process_creator(char *line)
-
+int
+process_creator (char *line)
 {
 
-   
-    char *cmd, *token, *rest;
-    int pposition, cposition;  /* process and commands position */
-    pposition = cposition = 0; 
-    
-    token = strtok(line, "|");   /* split the line by  into proper number of processes */
-    while(token != NULL) {
-        rest = token;
-        while((cmd = strtok_r(rest, "' '\t\r\a\n", &rest)) != NULL) {   /* split process string into proper arguments */
-            process[pposition].commands[cposition] = cmd;
-            ++cposition;
-            if(cposition > 15) {
-                perror("arguments limit exceed");
-            }
-        }
-        process[pposition].commands[cposition] = '\0';
-        cposition = 0;
-        ++pposition;
-        if(pposition > 10) {
-            perror("processes limit exceeds");
-        }
-        
-        token = strtok(NULL, "|");
-    } 
+  char *cmd, *token, *rest;
+  int pposition, cposition;	/* process and commands position */
+  pposition = cposition = 0;
 
-    return pposition;  /* return number of processes */
+  token = strtok (line, "|");	/* split the line by  into proper number of processes */
+  while (token != NULL)
+    {
+      rest = token;
+      while ((cmd = strtok_r (rest, "' '\t\r\a\n", &rest)) != NULL)
+	{			/* split process string into proper arguments */
+	  process[pposition].commands[cposition] = cmd;
+	  ++cposition;
+	  if (cposition > 15)
+	    {
+	      perror ("arguments limit exceed");
+	    }
+	}
+      process[pposition].commands[cposition] = '\0';
+      cposition = 0;
+      ++pposition;
+      if (pposition > 10)
+	{
+	  perror ("processes limit exceeds");
+	}
+
+      token = strtok (NULL, "|");
+    }
+  return pposition;		/* return number of processes */
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-   
-
-
-
-
-
-    
-
-
-
-
-
-
-     
-
-      
-
-
-
-
-
-
-
-      
-
-
-
