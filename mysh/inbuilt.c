f7fe90728ae1327383ef86ff1dd9da4f633fdaf2
neerajkarunakaran <neerajkarunakaran.c@gmail.com>
2017-06-28 02:10:36 +0530

    final commit

--- mysh/inbuilt.c
+++ mysh/inbuilt.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -9,192 +8,63 @@
 #include <sys/stat.h>
 #include <wait.h>
 #include <ctype.h>
-char * get_work_directory(void)  //get current working directory
+char *
+get_work_directory (void)	//get current working directory
 {
-    char *current_directory;
-    char buf[1024];
-    current_directory = getcwd(buf, 1024);
-    return current_directory;
+  char *current_directory;
+  char buf[1024];
+  current_directory = getcwd (buf, 1024);
+  return current_directory;
 }
 
-void init_sh(void)  //initialise the myshell 
+void
+init_sh (void)			//initialise the myshell 
 {
-    chdir("/home");
+  chdir ("/home");
 }
 
-void cd_fun(void) //change working directory`
-{   
-
-
-    if(process[0].commands[1] == NULL) {
-        fprintf(stderr, "usage: cd \n");
+void
+cd_fun (void)			//change working directory`
+{
+  if (process[0].commands[1] == NULL)
+    {
+      fprintf (stderr, "usage: cd \n");
     }
-    else {
-        chdir(process[0].commands[1]);
+  else
+    {
+      chdir (process[0].commands[1]);
     }
-
 }
 
-
-
-
-
-
-
-
-
-
-
-
-void exit_fun(void)  //exit command
+void
+exit_fun (void)			//exit command
 {
-    _exit(0);
+  _exit (0);
 }
 
-int select_cmd(void)
-{   
-    
+int
+select_cmd (void)
+{
 
-
-    if(strcmp(process[0].commands[0], "cd") == 0) {
-        cd_fun();
-        return 0;
-    }else if(strcmp(process[0].commands[0], "exit") == 0) {
-        exit_fun();
-        return 0;
+  if (strcmp (process[0].commands[0], "cd") == 0)
+    {
+      cd_fun ();
+      return 0;
     }
-    return 1;
-    
+  else if (strcmp (process[0].commands[0], "exit") == 0)
+    {
+      exit_fun ();
+      return 0;
+    }
+  return 1;
+
 }
 
-
-char *read_inputline(void)  //read input lines
+char *
+read_inputline (void)		//read input lines
 {
-   char *line = NULL;
-   ssize_t bufsize = 0;
-     getline(&line, &bufsize, stdin);  //get null terminated string
-    return line;
+  char *line = NULL;
+  ssize_t bufsize = 0;
+  getline (&line, &bufsize, stdin);	//get null terminated string
+  return line;
 }
-
-
-
-
-
-
-  
-
-
-
-   
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-    
-
-        
-        
-    
-        
-        
-      
-        
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
